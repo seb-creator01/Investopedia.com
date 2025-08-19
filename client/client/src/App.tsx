fe038cdb39a61a6efc00d8d988370f689a2d3328
seb-creator01 <precioussebastian70@gmail.com>
2025-08-19 15:24:52 +0100

    Create App.tsx

---
+++ client/client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Investorpedia</h1>
+      <p>This is your React app running!</p>
+    </div>
+  );
+};
